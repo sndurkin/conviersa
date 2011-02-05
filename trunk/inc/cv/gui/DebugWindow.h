ce966c8689fc4e8109fda90b768ab225ddbe30c2
sndurkin <sndurkin@a92f4fa4-ea95-11de-9d05-050bbbc75a16>
2011-02-05 20:16:43 +0000

    forgot to add files for DebugWindow :/

---
+++ trunk/inc/cv/gui/DebugWindow.h
@@ -0,0 +1,32 @@
+/************************************************************************
+*
+* The MIT License
+*
+* Copyright (c) 2007-2009 Conviersa Project
+*
+************************************************************************/
+
+#pragma once
+
+#include "cv/gui/OutputWindow.h"
+
+namespace cv { namespace gui {
+
+class DebugWindow : public OutputWindow
+{
+public:
+    DebugWindow(Session *pSession,
+                const QString &title = tr("Debug Window"),
+                const QSize &size = QSize(500, 300));
+    ~DebugWindow();
+
+    void giveFocus();
+
+    void onSendData(Event *pEvt);
+    void onReceivedData(Event *pEvt);
+
+    void onOutput(Event *pEvt);
+    void onDoubleClickLink(Event *pEvt);
+};
+
+} } // end namespaces
